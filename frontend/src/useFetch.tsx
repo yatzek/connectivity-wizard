e3db2763365012ac77564b111701c7694099e8ef
Jacek Szlachta <82675711+yatzek@users.noreply.github.com>
2023-10-05 19:39:02 +0100

    lower timeout to 500ms

--- frontend/src/useFetch.tsx
+++ frontend/src/useFetch.tsx
@@ -31,7 +31,7 @@ const useFetch = (url) => {
                     setError(err.message)
                 }
             })
-        }, 1000)
+        }, 500)
 
         return () => abortCont.abort();
 
