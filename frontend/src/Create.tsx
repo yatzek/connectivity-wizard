95743df99efa987d72f11c3a15da6139f62f7b2b
Jacek Szlachta <82675711+yatzek@users.noreply.github.com>
2023-10-05 09:56:26 +0100

    create blog page - post to backend

--- frontend/src/Create.tsx
+++ frontend/src/Create.tsx
@@ -1,14 +1,37 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom"
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+            // go to homepage
+            navigate('/');
+        })
+
+
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
@@ -32,11 +55,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
-
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                { !isPending && <button>Add blog</button> }
+                { isPending && <button disabled>Adding blog...</button> }
             </form>
         </div>
     );
